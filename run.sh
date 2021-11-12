5ff120d8344c28909ce746e56f2fd22948561e65
Kiyo <88574877+Kyxzy@users.noreply.github.com>
2021-11-12 16:14:22 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@ wget https://github.com/kubernetes/minikube/releases/download/v1.24.0/minikube-l
 mv minikube-linux-amd64 minikube
 chmod +x minikube
 mv minikube /usr/local/bin/minikube
-minikube start
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo chmod +x kubectl
 mv kubectl /user/local/bin/kubectl
