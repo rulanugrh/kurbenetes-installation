5e9a7a7c53109ec0895d54fd5a2cee24c6fce5e7
Kiyo <88574877+Kyxzy@users.noreply.github.com>
2021-11-17 15:53:06 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 echo "Author : Kiyotaka | Kyxzy"
-wget https://github.com/kubernetes/minikube/releases/download/v1.24.0/minikube-linux-amd64
-mv minikube-linux-amd64 minikube
-chmod +x minikube
-mv minikube /usr/local/bin/minikube
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo apt install curl wget -y
+sudo wget https://github.com/kubernetes/minikube/releases/download/v1.24.0/minikube-linux-amd64
+sudo mv minikube-linux-amd64 minikube
+sudo chmod +x minikube
+sudo mv minikube /usr/local/bin/minikube
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo chmod +x kubectl
-mv kubectl /user/local/bin/kubectl
+sudo mv kubectl /user/local/bin/kubectl
